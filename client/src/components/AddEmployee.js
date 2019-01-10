cfb7d31f0f9165408da803bcdbc2dacaecacf10a
Sophia Butterworth <sophiabutterworth@gmail.com>
2019-01-10 17:40:51 -0500

    adding new component to hold new employee additions

---
+++ client/src/components/AddEmployee.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import axios from 'axios';
+import ReactDOM from 'react-dom';
+
+class AddEmployee extends React.Component {
+    state = {
+        empName: "",
+        phoneNo: "",
+        sunAM: "",
+        sunPM: "",
+        monAM: "",
+        monPM: "",
+        tueAM: "",
+        tuePM: "",
+        wedAM: "",
+        wedPM: "",
+        thuAM: "",
+        thuPM: "",
+        friAM: "",
+        friPM: "",
+        satAM: "",
+        satPM: ""
+    };
+
+    handleInputChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        const employee = {
+            name: this.state.empName,
+            phoneNo: this.state.phoneNo,
+            sunAM: this.state.sunAM,
+            sunPM: this.state.sunPM,
+            monAM: this.state.monAM,
+            monPM: this.state.monPM,
+            tueAM: this.state.tueAM,
+            tuePM: this.state.tuePM,
+            wedAM: this.state.wedAM,
+            wedPM: this.state.wedPM,
+            thuAM: this.state.thuAM,
+            thuPM: this.state.thuPM,
+            friAM: this.state.friAM,
+            friPM: this.state.friPM,
+            satAM: this.state.satAM,
+            satPM: this.state.satPM
+        }
+
+        console.log(this.state);
+
+        axios.post('/api/newemployee', employee)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+            .catch(err => console.log(err));
+    }
+
+    render() {
+        return (
+            
+        )
+    }
+}
